15e80f9f6aba148f03316448deb7557ca2b57081
Tamago <yukotaniguchi@outlook.com>
2022-03-09 23:16:24 +1100

    button fixed

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const addAdvice=async()=>{
     const advice=document.querySelector('#advice');
     adviceNums.innerText=`Advice #${adviceText.id}`;
     advice.innerText=`"${adviceText.advice}"`;
+    button.style.boxShadow=''
 }
 
 const getAdvice=async()=>{
+    button.style.boxShadow='0px 0px 40px 0px var(--color-neon-green)'
     try{
         const config={headers:{Accept:'application/json'}};
         const res = await axios.get("https://api.adviceslip.com/advice",config);
